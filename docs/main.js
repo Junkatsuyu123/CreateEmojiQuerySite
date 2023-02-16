9e5579b82ab853d89785b910c4fabc756d87b89a
Junkatsuyu123 <108163348+Junkatsuyu123@users.noreply.github.com>
2023-02-16 22:55:32 +0900

    test

--- docs/main.js
+++ docs/main.js
@@ -16,7 +16,7 @@ const TXT = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".spl
 var express = require("express");
 var app = express();
 
-app.use( express.static( __dirname) );
+app.use( express.static(  __dirname + '/docs' ) );
 
 var port = process.env.PORT || 8080;
 app.listen( port );
