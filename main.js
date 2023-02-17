c0f64b4dcf96fda41c5ab40b2fdaa66a139644ff
Junkatsuyu123 <108163348+Junkatsuyu123@users.noreply.github.com>
2023-02-17 20:15:28 +0900

    test

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ app.post("/", function (request, response) {
                 if (fs.existsSync(file_name)) {
                     response.download(file_name);
                 }
-                return response.sendFile(__dirname + '/docs' + "/index.html");
+                //return response.sendFile(__dirname + '/docs' + "/index.html");
                 //return response.sendFile('https://junkatsuyu123.github.io/CreateEmojiQuerySite/');
           })
       }
