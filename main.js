1dfb025d561220d0b459a37a5ebd154acf79d08d
Junkatsuyu123 <108163348+Junkatsuyu123@users.noreply.github.com>
2023-02-17 21:45:54 +0900

    test

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ const TXT = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".spl
 var express = require("express");
 var app = express();
 
-//app.use( express.static(  __dirname + '/docs' ) );
+app.use( express.static(  __dirname + '/docs' ) );
 
-app.use( express.static('https://junkatsuyu123.github.io/CreateEmojiQuerySite' + '/docs' ) );
+//app.use( express.static('https://junkatsuyu123.github.io/CreateEmojiQuerySite' + '/docs' ) );
 
 var port = process.env.PORT || 8080;
 app.listen( port );
@@ -45,7 +45,45 @@ app.post("/", function (request, response) {
                 });*/
                 //var text = fs.readFileSync(__dirname+"\\query_insert.txt", 'utf8' , (err) => {
                 //var text = fs.readFileSync('https://junkatsuyu123.github.io/CreateEmojiQuerySite/'+"query_insert.txt", 'utf8' , (err) => {
-                
+                var text = fs.readFileSync('https://junkatsuyu123.github.io/CreateEmojiQuerySite/'+"Query/insert.txt", 'utf8' , (err) => {
+                    if (err) {
+                        console.log(err.stack);
+                    }
+                    else {
+                        console.log('Done.');
+                    }
+                })
+                var flag = false;
+                for (let i = 0; i < Number(num); i++){
+                    const current_date = new Date();
+                    const old_date = new Date(2000, 1, 1, 9, 0, 0);
+                    var date = (current_date - old_date).toString(36).padStart(8, '0') + TXT[Math.floor(Math.random() * TXT.length)] + TXT[Math.floor(Math.random() * TXT.length)];
+                    while (IDList.includes(date)) {
+                        date = (current_date - old_date).toString(36).padStart(8, '0') + TXT[Math.floor(Math.random() * TXT.length)] + TXT[Math.floor(Math.random() * TXT.length)];
+                    }
+                    var f_name;
+                    if (i == 0) {
+                        f_name = name + '_'+ date;
+                    }
+                    text = text.replace("{USERNAME}", name);
+                    text = text.replace("{ID}",date);
+                    //file_name = __dirname + "/CreateQuery/" + "query_insert_" + f_name + ".txt";
+                    file_name = 'https://junkatsuyu123.github.io/CreateEmojiQuerySite/' + "CreateQuery/" + "query_insert_" + f_name + ".txt";
+                    if (fs.existsSync( file_name )) {
+                        fs.appendFile(file_name, text,  'utf-8',(err) => {
+                            if (err) throw err;
+                            console.log('正常に書き込みが完了しました');
+                        });
+                    }
+                    else {
+                        fs.writeFile(file_name, text, 'utf-8',(err) => {
+                            if (err) throw err;
+                            flag = true;
+                            console.log('正常に書き込みが完了しました');
+                        });
+                    }
+                    
+                }
             }
         })
             .on('end', function () {
@@ -53,7 +91,7 @@ app.post("/", function (request, response) {
                     response.download(file_name);
                 }*/
                 //return response.sendFile(__dirname + '/docs' + "/index.html");
-                return response.sendFile('https://junkatsuyu123.github.io/CreateEmojiQuerySite/index.html');
+                //return response.sendFile('https://junkatsuyu123.github.io/CreateEmojiQuerySite/');
           })
       }
 });
